818365366859179323c96ef2d2059e1c12244ec1
ap2144839 <ap2144839@gmail.com>
2018-06-13 08:53:36 +0300

    refactoring-to-oop - refactoring classes com.collections.WorkWithList, com.peterson.util.Util - deleting classes com.peterson.collection.WorkWithArrayList, com.peterson.collection.WorkWithLinkedList https://trello.com/c/smZCxnIr/5-refactoring-to-oop

--- src/com/peterson/collection/WorkWithList.java
+++ src/com/peterson/collection/WorkWithList.java
@@ -1,8 +1,21 @@
 package com.peterson.collection;
 
+import java.util.List;
+
 /**
  * Created by user on 21.05.2018.
  */
-public abstract class WorkWithList {
+public class WorkWithList {
+
+    public static List<Integer> fillList(List<Integer> list, int size){
+        for (int i = 0; i < size; i++){
+            list.add(1000);
+        }
+        return list;
+    }
+
+    public static void deleteElementList (List<Integer> list, int index){
+        list.remove(index);
+    }
 
 }
--- src/com/peterson/util/Util.java
+++ src/com/peterson/util/Util.java
@@ -1,10 +1,10 @@
 package com.peterson.util;
 
+import com.peterson.collection.WorkWithList;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import com.peterson.collection.WorkWithArrayList;
-import com.peterson.collection.WorkWithLinkedList;
 
 /**
  * Created by user on 23.05.2018.
@@ -15,25 +15,24 @@ public class Util {
         //ArrayList=====================================================================================================
         //FILLING AND DELETING EXPERIMENT FROM ARRAYLIST OF 100 ELEMENTS------------------------------------------------
         //Filling ArrayList of 100 elements
-        WorkWithArrayList workWithArrayList = new WorkWithArrayList();
         List<Integer> arrayList1 = new ArrayList<>();
         long start = System.currentTimeMillis();
-        List<Integer> list1 = workWithArrayList.fillList(arrayList1, 100);
+        List<Integer> list1 = WorkWithList.fillList(arrayList1, 100);
         long stop = System.currentTimeMillis();
         System.out.println("Filling ArrayList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting last element from ArrayList of 100 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list1, 99);
+        WorkWithList.deleteElementList(list1, 99);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from ArrayList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from ArrayList of 100 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list1, 50);
+        WorkWithList.deleteElementList(list1, 50);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from ArrayList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting first element from ArrayList of 100 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list1, 0);
+        WorkWithList.deleteElementList(list1, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from ArrayList of 100 elements takes: " + (stop-start) + " ms");
 
@@ -41,22 +40,22 @@ public class Util {
         //Filling ArrayList of 1,000 elements
         List<Integer> arrayList2 = new ArrayList<>();
         start = System.currentTimeMillis();
-        List<Integer> list2 = workWithArrayList.fillList(arrayList2, 1000);
+        List<Integer> list2 = WorkWithList.fillList(arrayList2, 1000);
         stop = System.currentTimeMillis();
         System.out.println("Filling ArrayList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting last element from ArrayList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list2, 999);
+        WorkWithList.deleteElementList(list2, 999);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from ArrayList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from ArrayList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list2, 500);
+        WorkWithList.deleteElementList(list2, 500);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from ArrayList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting first element from ArrayList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list2, 0);
+        WorkWithList.deleteElementList(list2, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from ArrayList of 1,000 elements takes: " + (stop-start) + " ms");
 
@@ -64,47 +63,46 @@ public class Util {
         //Filling ArrayList of 1,000,000 elements
         List<Integer> arrayList3 = new ArrayList<>();
         start = System.currentTimeMillis();
-        List<Integer> list3 = workWithArrayList.fillList(arrayList3, 1000000);
+        List<Integer> list3 = WorkWithList.fillList(arrayList3, 1000000);
         stop = System.currentTimeMillis();
         System.out.println("Filling ArrayList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting last element from ArrayList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list3, 999999);
+        WorkWithList.deleteElementList(list3, 999999);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from ArrayList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from ArrayList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list3, 500000);
+        WorkWithList.deleteElementList(list3, 500000);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from ArrayList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting first element from ArrayList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithArrayList.deleteElementList(list3, 0);
+        WorkWithList.deleteElementList(list3, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from ArrayList of 1,000,000 elements takes: " + (stop-start) + " ms");
 
         //LinkedList====================================================================================================
         //FILLING AND DELETING EXPERIMENT FROM LINKEDLIST OF 100 ELEMENTS-----------------------------------------------
         //Filling LinkedList of 100 elements
-        WorkWithLinkedList workWithLinkedList = new WorkWithLinkedList();
         List<Integer> linkedList4 = new LinkedList<>();
         start = System.currentTimeMillis();
-        List<Integer> list4 = workWithLinkedList.fillList(linkedList4, 100);
+        List<Integer> list4 = WorkWithList.fillList(linkedList4, 100);
         stop = System.currentTimeMillis();
         System.out.println("Filling LinkedList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting last element from LinkedList of 100 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list4, 99);
+        WorkWithList.deleteElementList(list4, 99);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from LinkedList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from LinkedList of 100 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list4, 50);
+        WorkWithList.deleteElementList(list4, 50);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from LinkedList of 100 elements takes: " + (stop-start) + " ms");
         //Deleting first element from LinkedList of 100 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list4, 0);
+        WorkWithList.deleteElementList(list4, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from LinkedList of 100 elements takes: " + (stop-start) + " ms");
 
@@ -112,22 +110,22 @@ public class Util {
         //Filling LinkedList of 1,000 elements
         List<Integer> linkedList5 = new LinkedList<>();
         start = System.currentTimeMillis();
-        List<Integer> list5 = workWithLinkedList.fillList(linkedList5, 1000);
+        List<Integer> list5 = WorkWithList.fillList(linkedList5, 1000);
         stop = System.currentTimeMillis();
         System.out.println("Filling LinkedList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting last element from LinkedList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list5, 999);
+        WorkWithList.deleteElementList(list5, 999);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from LinkedList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from LinkedList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list5, 500);
+        WorkWithList.deleteElementList(list5, 500);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from LinkedList of 1,000 elements takes: " + (stop-start) + " ms");
         //Deleting first element from LinkedList of 1,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list5, 0);
+        WorkWithList.deleteElementList(list5, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from LinkedList of 1,000 elements takes: " + (stop-start) + " ms");
 
@@ -135,22 +133,22 @@ public class Util {
         //Filling ArrayList of 1,000,000 elements
         List<Integer> linkedList6 = new LinkedList<>();
         start = System.currentTimeMillis();
-        List<Integer> list6 = workWithLinkedList.fillList(linkedList6, 1000000);
+        List<Integer> list6 = WorkWithList.fillList(linkedList6, 1000000);
         stop = System.currentTimeMillis();
         System.out.println("Filling LinkedList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting last element from LinkedList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list6, 999999);
+        WorkWithList.deleteElementList(list6, 999999);
         stop = System.currentTimeMillis();
         System.out.println("Deleting last element from LinkedList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting middle element from LinkedList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list6, 500000);
+        WorkWithList.deleteElementList(list6, 500000);
         stop = System.currentTimeMillis();
         System.out.println("Deleting middle element from LinkedList of 1,000,000 elements takes: " + (stop-start) + " ms");
         //Deleting first element from LinkedList of 1,000,000 elements
         start = System.currentTimeMillis();
-        workWithLinkedList.deleteElementList(list6, 0);
+        WorkWithList.deleteElementList(list6, 0);
         stop = System.currentTimeMillis();
         System.out.println("Deleting first element from LinkedList of 1,000,000 elements takes: " + (stop-start) + " ms");
 
